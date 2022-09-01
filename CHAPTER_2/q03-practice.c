c4ca844a18816bda618c1c2ae4b230b418e8c14a
HibeeK <akintolauser@gmail.com>
2022-09-01 12:57:49 +0000

    All chapter 1and2 practice set done

---
+++ CHAPTER_2/q03-practice.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+	int a = 100;
+
+	printf("The remainder is: %d\n", a%97);
+
+	return 0;
+	
+
+
+}
---
+++ CHAPTER_2/tempCodeRunnerFile.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+	int a = 100;
+
+	printf("The reminder is: %d\n", a%97);
+	
+	return 0;
+	
+
+
+}
